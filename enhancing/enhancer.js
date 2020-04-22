4b8a462448020896a6e92e58cde1c593639c5f59
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-04-22 13:06:55 -0600

    added success and repair tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,7 +6,10 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  return {
+    ...item,
+    enhancement: item.enhancement >= 20 ? item.enhancement : item.enhancement + 1
+  };
 }
 
 function fail(item) {
@@ -14,7 +17,10 @@ function fail(item) {
 }
 
 function repair(item) {
-  return { ...item };
+  return {
+    ...item,
+    durability: 100
+  };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,24 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+const item = {
+    name: "Rusty Soup Ladle",
+    durability: 20,
+    enhancement: 19
+}
+
+describe("enhancer.js", () => {
+    describe("repair(item)", () => {
+        it("item durability is 100 after repair(item)", () => {
+            expect(enhancer.repair(item).durability).toBe(100);
+        })
+    })
+    describe("succeed(item)", () => {
+        it("item enhancement is increased by 1 after succeed(item)", () => {
+            expect(enhancer.succeed(item).enhancement).toBe(20);
+        })
+        it("if enhancement level is 20, the enhancement level is not changed", () => {
+            expect(enhancer.succeed(item).enhancement).toBe(20);
+        })
+    })
+})
